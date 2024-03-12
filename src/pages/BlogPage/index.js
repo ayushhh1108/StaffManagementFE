df52e7bc8ed9e15bf286cab799757ec04d6a37fd
ayush@4718 <ayushhirapara1108@gmail.com>
2024-03-12 18:09:21 +0530

    add add-buttons

--- src/pages/BlogPage/index.js
+++ src/pages/BlogPage/index.js
@@ -152,10 +152,16 @@ function BlogPage() {
       component="main"
       sx={{ flexGrow: 1, p: 3, mt: 8 }}
     >
-      <Container className="pt-[60px] menu-list-container">
+      <Container className="pt-[60px] menu-list-container text-left">
+        <button
+          type="button"
+          className="text-white bg-[#1e6c89] hover:bg-[#164e63] font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-3"
+        >
+          Add Blog
+        </button>
         <EnhancedTable
           cellData={headerData}
-          rowItems={["no", "title", "description", "metaTitle","metaDesc"]}
+          rowItems={["no", "title", "description", "metaTitle", "metaDesc"]}
           rowData={allData?.map((item, index) => ({ ...item, no: 1 + index }))}
         />
       </Container>
