773132d9080377ef0f5dd1ad81a275c70efcfd61
ayush@4718 <ayushhirapara1108@gmail.com>
2024-03-21 17:15:02 +0530

    career and add career

--- src/pages/AddCareerPage/AddCareerPageHooks.js
+++ src/pages/AddCareerPage/AddCareerPageHooks.js
@@ -2,12 +2,33 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function AddCareerPageHooks() {
+  const [data, setData] = useState();
   const navigate = useNavigate();
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
+  const handleInputChange = (idOrEvent, val) => {
+    const event = isEventBased(idOrEvent) ? idOrEvent : null;
+    const key =
+      event?.target?.name === "rating"
+        ? "rating"
+        : event?.target?.id || idOrEvent;
+    let value = event ? event.target.value : val;
+    setData({ ...data, [key]: value });
+  };
+
+  const isEventBased = (input) => !!input?.target?.id;
+
+  const handleSubmit = () => {
+    console.log("handleSubmit", data);
+    // dispatch(loginSubmit(creds,navigate))
+  };
+
   return {
     navigate,
+    handleSubmit,
+    handleInputChange,
+    data,
   };
 }
--- src/pages/AddCareerPage/index.js
+++ src/pages/AddCareerPage/index.js
@@ -1,16 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import "./index.scss";
 import AddCareerPageHooks from "./AddCareerPageHooks";
 import { Box, Typography } from "@mui/material";
-import Dropzone from "../../components/DropZone";
 import TextInput from "../../components/TextInput";
-import ClassicTextEditorr from "../../components/ClassicTextEditor";
-import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
-import SelectInput from "../../components/SelectInput";
 
 function AddCareerPage() {
-  const { navigate } = AddCareerPageHooks();
+  const { navigate, handleSubmit, handleInputChange, data } =
+    AddCareerPageHooks();
 
   return (
     <Box component="main" sx={{ flexGrow: 1, p: 3, mt: 8, maxWidth: "100%" }}>
@@ -18,15 +14,52 @@ function AddCareerPage() {
         <Typography variant="h5" className="mb-5 form-label">
           Add Career{" "}
         </Typography>
-        <TextInput label={"Degination"} isRequire id={"degination"} />
-        <TextInput label={"Department"} isRequire id={"department"} />
-        <TextInput label={"Experience"} isRequire id={"experience"} />
-        <TextInput label={"Location"} isRequire id={"location"} />
-        <TextInput label={"No Of Vacancy"} isRequire id={"no_vacancy"} />
-        <TextInput label={"Description"} isRequire id={"description"} />
+        <TextInput
+          label={"Degination"}
+          isRequire
+          id={"degination"}
+          handleChanges={handleInputChange}
+          value={data?.degination}
+        />
+        <TextInput
+          label={"Department"}
+          isRequire
+          id={"department"}
+          handleChanges={handleInputChange}
+          value={data?.department}
+        />
+        <TextInput
+          label={"Experience"}
+          isRequire
+          id={"experience"}
+          handleChanges={handleInputChange}
+          value={data?.experience}
+        />
+        <TextInput
+          label={"Location"}
+          isRequire
+          id={"location"}
+          handleChanges={handleInputChange}
+          value={data?.location}
+        />
+        <TextInput
+          label={"No Of Vacancy"}
+          isRequire
+          id={"no_vacancy"}
+          handleChanges={handleInputChange}
+          value={data?.no_vacancy}
+        />
+        <TextInput
+          label={"Description"}
+          isRequire
+          id={"description"}
+          handleChanges={handleInputChange}
+          value={data?.description}
+        />
 
         <button
           type="button"
+          onClick={handleSubmit}
           className="text-white bg-[#1e6c89] hover:bg-[#164e63] font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
         >
           Save
