b6d381d4b4d75604b6fd19ef21d7174e28a4d93b
ayush@4718 <ayushhirapara1108@gmail.com>
2024-03-22 17:22:16 +0530

    add-construction page done

--- src/pages/AddConstructionPage/index.js
+++ src/pages/AddConstructionPage/index.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import "./index.scss";
-
 import AddConstructionHooks from "./AddConstructionHooks";
 import { Box, Typography } from "@mui/material";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
@@ -10,7 +9,8 @@ import TextInput from "../../components/TextInput";
 import SelectInput from "../../components/SelectInput";
 
 function AddConstructionPage() {
-  const { navigate } = AddConstructionHooks();
+  const { navigate, handleSubmit, handleInputChange, data } =
+    AddConstructionHooks();
 
   return (
     <Box component="main" sx={{ flexGrow: 1, p: 3, mt: 8, maxWidth: "100%" }}>
@@ -18,18 +18,40 @@ function AddConstructionPage() {
         <Typography variant="h5" className="mb-5 form-label">
           Add Construction Process{" "}
         </Typography>
-        <TextInput label={"Title"} isRequire id={"title"} />
-        <TextInput label={"Meta Title "} isRequire id={"meta_title"} />
-        <TextInput label={"Meta Keywords "} isRequire id={"meta_keywords"} />
+        <TextInput
+          label={"Title"}
+          isRequire
+          id={"title"}
+          handleChanges={handleInputChange}
+          value={data?.title}
+        />
+        <TextInput
+          label={"Meta Title "}
+          isRequire
+          id={"meta_title"}
+          handleChanges={handleInputChange}
+          value={data?.meta_title}
+        />
+        <TextInput
+          label={"Meta Keywords "}
+          isRequire
+          id={"meta_keywords"}
+          handleChanges={handleInputChange}
+          value={data?.meta_keywords}
+        />
         <TextInput
           label={"Meta Description "}
           isRequire
           id={"meta_description"}
+          handleChanges={handleInputChange}
+          value={data?.meta_description}
         />
         <SelectInput
           label={"Image Position"}
           isRequire
           id={"position"}
+          handleChange={handleInputChange}
+          value={data?.position}
           options={[
             { label: "Left", value: "left" },
             { label: "Right", value: "right" },
@@ -43,8 +65,10 @@ function AddConstructionPage() {
               // You can store the "editor" and use when it is needed.
               console.log("Editor is ready to use!", editor);
             }}
-            onChange={(event) => {
-              console.log(event);
+            onChange={(event, editor) => {
+              handleInputChange({
+                target: { value: editor?.getData(), id: "editor_desc" },
+              });
             }}
             onBlur={(event, editor) => {
               console.log("Blur.", editor);
@@ -55,11 +79,16 @@ function AddConstructionPage() {
           />
         </div>
         <div className="upload-file-div mb-6 flex justify-between">
-          <Dropzone title={"Upload Image"} />
+          <Dropzone
+            title={"Upload Image"}
+            id="upload_image"
+            onChanges={handleInputChange}
+          />
         </div>
 
         <button
           type="button"
+          onClick={handleSubmit}
           className="text-white bg-[#1e6c89] hover:bg-[#164e63] font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
         >
           Save
