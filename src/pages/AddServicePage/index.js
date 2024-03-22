a274dc1682322b04d57733595d8f3e7582e1a02f
ayush@4718 <ayushhirapara1108@gmail.com>
2024-03-22 13:52:47 +0530

    Add service page functionality done

--- src/pages/AddServicePage/index.js
+++ src/pages/AddServicePage/index.js
@@ -1,15 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import "./index.scss";
-
 import AddServiceHooks from "./AddServiceHooks";
 import { Box, Typography } from "@mui/material";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
-import Dropzone from "../../components/DropZone";
 import TextInput from "../../components/TextInput";
 
 function AddServicePage() {
-  const { navigate } = AddServiceHooks();
+  const { navigate, handleSubmit, handleInputChange, data } = AddServiceHooks();
 
   return (
     <Box component="main" sx={{ flexGrow: 1, p: 3, mt: 8, maxWidth: "100%" }}>
@@ -17,14 +15,40 @@ function AddServicePage() {
         <Typography variant="h5" className="mb-5 form-label">
           Add Service{" "}
         </Typography>
-        <TextInput label={"Header"} isRequire id={"header"} />
-        <TextInput label={"Title"} isRequire id={"title"} />
-        <TextInput label={"Meta Title"} isRequire id={"meta_title"} />
-        <TextInput label={"Meta Keywords"} isRequire id={"meta_keywords"} />
+        <TextInput
+          label={"Header"}
+          isRequire
+          id={"header"}
+          handleChanges={handleInputChange}
+          value={data?.header}
+        />
+        <TextInput
+          label={"Title"}
+          isRequire
+          id={"title"}
+          handleChanges={handleInputChange}
+          value={data?.title}
+        />
+        <TextInput
+          label={"Meta Title"}
+          handleChanges={handleInputChange}
+          value={data?.meta_title}
+          isRequire
+          id={"meta_title"}
+        />
+        <TextInput
+          label={"Meta Keywords"}
+          handleChanges={handleInputChange}
+          value={data?.meta_keywords}
+          isRequire
+          id={"meta_keywords"}
+        />
         <TextInput
           label={"Meta Descripion"}
           isRequire
           id={"meta_description"}
+          handleChanges={handleInputChange}
+          value={data?.meta_description}
         />
         <div className="add-menu-input w-1/2 mb-5">
           <CKEditor
@@ -34,8 +58,10 @@ function AddServicePage() {
               // You can store the "editor" and use when it is needed.
               console.log("Editor is ready to use!", editor);
             }}
-            onChange={(event) => {
-              console.log(event);
+            onChange={(event, editor) => {
+              handleInputChange({
+                target: { value: editor?.getData(), id: "editor_desc" },
+              });
             }}
             onBlur={(event, editor) => {
               console.log("Blur.", editor);
@@ -48,6 +74,7 @@ function AddServicePage() {
 
         <button
           type="button"
+          onClick={handleSubmit}
           className="text-white bg-[#1e6c89] hover:bg-[#164e63] font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
         >
           Save
