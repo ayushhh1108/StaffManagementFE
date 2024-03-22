9e7950999c6fe22a6eb3c52706a32fa203701ade
ayush@4718 <ayushhirapara1108@gmail.com>
2024-03-22 17:13:14 +0530

    add-director page done

--- src/pages/AddDirectorPage/index.js
+++ src/pages/AddDirectorPage/index.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import "./index.scss";
-
 import AddDirectorHooks from "./AddDirectorHooks";
 import { Box, Typography } from "@mui/material";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
@@ -9,7 +8,8 @@ import Dropzone from "../../components/DropZone";
 import TextInput from "../../components/TextInput";
 
 function AddDirectorPage() {
-  const { navigate } = AddDirectorHooks();
+  const { navigate, handleSubmit, handleInputChange, data } =
+    AddDirectorHooks();
 
   return (
     <Box component="main" sx={{ flexGrow: 1, p: 3, mt: 8, maxWidth: "100%" }}>
@@ -17,17 +17,54 @@ function AddDirectorPage() {
         <Typography variant="h5" className="mb-5 form-label">
           Add Director{" "}
         </Typography>
-        <TextInput label={"Title"} isRequire id={"title"} />
+        <TextInput
+          label={"Title"}
+          isRequire
+          id={"title"}
+          handleChanges={handleInputChange}
+          value={data?.title}
+        />
         <TextInput
           label={"Short Description"}
           isRequire
           id={"short_description"}
+          handleChanges={handleInputChange}
+          value={data?.short_description}
         />
-        <TextInput label={"Designation"} id={"designation"} />
-        <TextInput label={"Facebook"} isRequire id={"facebook"} />
-        <TextInput label={"Twiter"} isRequire id={"twiter"} />
-        <TextInput label={"Instagram"} isRequire id={"instagram"} />
-        <TextInput label={"Linkedin"} isRequire id={"linkedin"} />
+        <TextInput
+          label={"Designation"}
+          id={"designation"}
+          handleChanges={handleInputChange}
+          value={data?.designation}
+        />
+        <TextInput
+          label={"Facebook"}
+          isRequire
+          id={"facebook"}
+          handleChanges={handleInputChange}
+          value={data?.facebook}
+        />
+        <TextInput
+          label={"Twiter"}
+          isRequire
+          id={"twiter"}
+          handleChanges={handleInputChange}
+          value={data?.twiter}
+        />
+        <TextInput
+          label={"Instagram"}
+          isRequire
+          id={"instagram"}
+          handleChanges={handleInputChange}
+          value={data?.instagram}
+        />
+        <TextInput
+          label={"Linkedin"}
+          isRequire
+          id={"linkedin"}
+          handleChanges={handleInputChange}
+          value={data?.linkedin}
+        />
         <div className="add-menu-input w-1/2 mb-5">
           <CKEditor
             editor={ClassicEditor}
@@ -36,8 +73,10 @@ function AddDirectorPage() {
               // You can store the "editor" and use when it is needed.
               console.log("Editor is ready to use!", editor);
             }}
-            onChange={(event) => {
-              console.log(event);
+            onChange={(event, editor) => {
+              handleInputChange({
+                target: { value: editor?.getData(), id: "editor_desc" },
+              });
             }}
             onBlur={(event, editor) => {
               console.log("Blur.", editor);
@@ -48,11 +87,12 @@ function AddDirectorPage() {
           />
         </div>
         <div className="upload-file-div mb-6 flex justify-between">
-          <Dropzone title={"Image"} />
+          <Dropzone title={"Image"} id="image" onChanges={handleInputChange} />
         </div>
 
         <button
           type="button"
+          onClick={handleSubmit}
           className="text-white bg-[#1e6c89] hover:bg-[#164e63] font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
         >
           Save
