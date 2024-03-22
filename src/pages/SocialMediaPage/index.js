9fc07845b67b404c39191ee8950b4412951fef73
ayush@4718 <ayushhirapara1108@gmail.com>
2024-03-22 16:57:09 +0530

    social media page done

--- src/pages/SocialMediaPage/index.js
+++ src/pages/SocialMediaPage/index.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import "./index.scss";
 import SocialMediaHooks from "./SocialMediaHooks";
 import { Box, Typography } from "@mui/material";
-import Dropzone from "../../components/DropZone";
 import TextInput from "../../components/TextInput";
 
 function SocialMediaPage() {
-  const { navigate } = SocialMediaHooks();
+  const { navigate, handleSubmit, handleInputChange, data } =
+    SocialMediaHooks();
 
   return (
     <Box component="main" sx={{ flexGrow: 1, p: 3, mt: 8, maxWidth: "100%" }}>
@@ -14,14 +14,45 @@ function SocialMediaPage() {
         <Typography variant="h5" className="mb-5 form-label">
           Add Social Media{" "}
         </Typography>
-        <TextInput label={"Facebook"} isRequire id={"facebook"} />
-        <TextInput label={"Twiter"} isRequire id={"twiter"} />
-        <TextInput label={"Instagram"} isRequire id={"instagram"} />
-        <TextInput label={"Linkedin"} isRequire id={"linkedin"} />
-        <TextInput label={"Youtube"} isRequire id={"youtube"} />
+        <TextInput
+          label={"Facebook"}
+          isRequire
+          id={"facebook"}
+          handleChanges={handleInputChange}
+          value={data?.facebook}
+        />
+        <TextInput
+          label={"Twiter"}
+          isRequire
+          id={"twiter"}
+          handleChanges={handleInputChange}
+          value={data?.twiter}
+        />
+        <TextInput
+          label={"Instagram"}
+          isRequire
+          id={"instagram"}
+          handleChanges={handleInputChange}
+          value={data?.instagram}
+        />
+        <TextInput
+          label={"Linkedin"}
+          isRequire
+          id={"linkedin"}
+          handleChanges={handleInputChange}
+          value={data?.linkedin}
+        />
+        <TextInput
+          label={"Youtube"}
+          isRequire
+          id={"youtube"}
+          handleChanges={handleInputChange}
+          value={data?.youtube}
+        />
 
         <button
           type="button"
+          onClick={handleSubmit}
           className="text-white bg-[#1e6c89] hover:bg-[#164e63] font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
         >
           Save
