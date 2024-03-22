9399a3eef9ade234a1f8a2ab5d61d597dee9112e
ayush@4718 <ayushhirapara1108@gmail.com>
2024-03-22 16:54:07 +0530

    site address page done

--- src/pages/SiteAddressPage/index.js
+++ src/pages/SiteAddressPage/index.js
@@ -1,13 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import "./index.scss";
-
 import SiteAddressHooks from "./SiteAddressHooks";
 import { Box, Typography } from "@mui/material";
-import Dropzone from "../../components/DropZone";
 import TextInput from "../../components/TextInput";
 
 function SiteAddressPage() {
-  const { navigate } = SiteAddressHooks();
+  const { navigate, handleSubmit, handleInputChange, data } =
+    SiteAddressHooks();
 
   return (
     <Box component="main" sx={{ flexGrow: 1, p: 3, mt: 8, maxWidth: "100%" }}>
@@ -15,16 +14,59 @@ function SiteAddressPage() {
         <Typography variant="h5" className="mb-5 form-label">
           Add Address{" "}
         </Typography>
-        <TextInput label={"Address"} isRequire id={"address"} />
-        <TextInput label={"City"} isRequire id={"city"} />
-        <TextInput label={"State"} isRequire id={"state"} />
-        <TextInput label={"Pincode"} isRequire id={"pincode"} />
-        <TextInput label={"Mobile"} isRequire id={"mobile"} />
-        <TextInput label={"Email"} isRequire id={"email"} />
-        <TextInput label={"Timing"} isRequire id={"timing"} />
+        <TextInput
+          label={"Address"}
+          isRequire
+          id={"address"}
+          handleChanges={handleInputChange}
+          value={data?.address}
+        />
+        <TextInput
+          label={"City"}
+          isRequire
+          id={"city"}
+          handleChanges={handleInputChange}
+          value={data?.city}
+        />
+        <TextInput
+          label={"State"}
+          isRequire
+          id={"state"}
+          handleChanges={handleInputChange}
+          value={data?.state}
+        />
+        <TextInput
+          label={"Pincode"}
+          isRequire
+          id={"pincode"}
+          handleChanges={handleInputChange}
+          value={data?.pincode}
+        />
+        <TextInput
+          label={"Mobile"}
+          isRequire
+          id={"mobile"}
+          handleChanges={handleInputChange}
+          value={data?.mobile}
+        />
+        <TextInput
+          label={"Email"}
+          isRequire
+          id={"email"}
+          handleChanges={handleInputChange}
+          value={data?.email}
+        />
+        <TextInput
+          label={"Timing"}
+          isRequire
+          id={"timing"}
+          handleChanges={handleInputChange}
+          value={data?.timing}
+        />
 
         <button
           type="button"
+          onClick={handleSubmit}
           className="text-white bg-[#1e6c89] hover:bg-[#164e63] font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
         >
           Save
