f6495dd8ac311e0fb673d93ed434baed03f4e0ca
ayush@4718 <ayushhirapara1108@gmail.com>
2024-03-22 18:00:38 +0530

    add cms with us page done

--- src/pages/AddCMSPage/AddCMSHooks.js
+++ src/pages/AddCMSPage/AddCMSHooks.js
@@ -2,12 +2,32 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function AddCMSHooks() {
+  const [data, setData] = useState();
   const navigate = useNavigate();
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
+  const handleInputChange = (id, val) => {
+    const event = isEventBased(id) ? id : null;
+    const key = event ? event.target.id : id;
+    let value = event ? event.target.value : val;
+    const isUpload = key === "icon_image";
+    value = isUpload && event ? event.target.files[0] : value;
+    setData({ ...data, [key]: value });
+  };
+
+  const isEventBased = (input) => !!input?.target?.id;
+
+  const handleSubmit = () => {
+    console.log("handleSubmit", data);
+    // dispatch(loginSubmit(creds,navigate))
+  };
+
   return {
     navigate,
+    handleSubmit,
+    handleInputChange,
+    data,
   };
 }
--- src/pages/AddCMSPage/index.js
+++ src/pages/AddCMSPage/index.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import "./index.scss";
-
 import AddCMSHooks from "./AddCMSHooks";
 import { Box, Typography } from "@mui/material";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
@@ -10,7 +9,7 @@ import TextInput from "../../components/TextInput";
 import SelectInput from "../../components/SelectInput";
 
 function AddCMSPage() {
-  const { navigate } = AddCMSHooks();
+  const { navigate, handleSubmit, handleInputChange, data } = AddCMSHooks();
 
   return (
     <Box component="main" sx={{ flexGrow: 1, p: 3, mt: 8, maxWidth: "100%" }}>
@@ -22,6 +21,8 @@ function AddCMSPage() {
           label={"Position"}
           isRequire
           id={"position"}
+          handleChange={handleInputChange}
+          value={data?.position}
           options={[
             { label: "Left", value: "left" },
             { label: "Right", value: "right" },
@@ -30,32 +31,68 @@ function AddCMSPage() {
         <SelectInput
           label={"Type"}
           isRequire
-          id={"position"}
+          id={"type"}
+          handleChange={handleInputChange}
+          value={data?.type}
           options={[
             { label: "Page", value: "page" },
             { label: "Url", value: "url" },
           ]}
         />
-        <TextInput label={"Page Name"} isRequire id={"page_name"} />
-        <TextInput label={"Page Title"} isRequire id={"page_title"} />
-        <TextInput label={"Short Description"} isRequire id={"short_desc"} />
-        <TextInput label={"Meta Title "} isRequire id={"meta_title"} />
-        <TextInput label={"Meta Keywords "} isRequire id={"meta_keywords"} />
+        <TextInput
+          label={"Page Name"}
+          isRequire
+          id={"page_name"}
+          handleChanges={handleInputChange}
+          value={data?.page_name}
+        />
+        <TextInput
+          label={"Page Title"}
+          isRequire
+          id={"page_title"}
+          handleChanges={handleInputChange}
+          value={data?.page_title}
+        />
+        <TextInput
+          label={"Short Description"}
+          isRequire
+          id={"short_desc"}
+          handleChanges={handleInputChange}
+          value={data?.short_desc}
+        />
+        <TextInput
+          label={"Meta Title "}
+          isRequire
+          id={"meta_title"}
+          handleChanges={handleInputChange}
+          value={data?.meta_title}
+        />
+        <TextInput
+          label={"Meta Keywords "}
+          isRequire
+          id={"meta_keywords"}
+          handleChanges={handleInputChange}
+          value={data?.meta_keywords}
+        />
         <TextInput
           label={"Meta Description "}
           isRequire
           id={"meta_description"}
+          handleChanges={handleInputChange}
+          value={data?.meta_description}
         />
         <div className="add-menu-input w-1/2 mb-5">
           <CKEditor
             editor={ClassicEditor}
-            data=""
+            data={data?.editor_desc}
             onReady={(editor) => {
               // You can store the "editor" and use when it is needed.
               console.log("Editor is ready to use!", editor);
             }}
-            onChange={(event) => {
-              console.log(event);
+            onChange={(event, editor) => {
+              handleInputChange({
+                target: { value: editor?.getData(), id: "editor_desc" },
+              });
             }}
             onBlur={(event, editor) => {
               console.log("Blur.", editor);
@@ -66,11 +103,16 @@ function AddCMSPage() {
           />
         </div>
         <div className="upload-file-div mb-6 flex justify-between">
-          <Dropzone title={"Icon Image"} />
+          <Dropzone
+            title={"Icon Image"}
+            id="icon_image"
+            onChanges={handleInputChange}
+          />
         </div>
 
         <button
           type="button"
+          onClick={handleSubmit}
           className="text-white bg-[#1e6c89] hover:bg-[#164e63] font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
         >
           Save
