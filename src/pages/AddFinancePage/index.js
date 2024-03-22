3ee6554a4554aad566b0f51a58f059bf29d8a1a2
ayush@4718 <ayushhirapara1108@gmail.com>
2024-03-22 13:51:57 +0530

    Finance page functionality done

--- src/pages/AddFinancePage/index.js
+++ src/pages/AddFinancePage/index.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import "./index.scss";
-
 import AddFinanceHooks from "./AddFinanceHooks";
 import { Box, Typography } from "@mui/material";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
@@ -9,7 +8,7 @@ import Dropzone from "../../components/DropZone";
 import TextInput from "../../components/TextInput";
 
 function AddFinancePage() {
-  const { navigate } = AddFinanceHooks();
+  const { navigate, handleSubmit, handleInputChange, data } = AddFinanceHooks();
 
   return (
     <Box component="main" sx={{ flexGrow: 1, p: 3, mt: 8, maxWidth: "100%" }}>
@@ -17,13 +16,33 @@ function AddFinancePage() {
         <Typography variant="h5" className="mb-5 form-label">
           Add Finance{" "}
         </Typography>
-        <TextInput label={"Title"} isRequire id={"title"} />
-        <TextInput label={"Meta Title"} isRequire id={"meta_title"} />
-        <TextInput label={"Meta Keywords"} isRequire id={"meta_keywords"} />
+        <TextInput
+          label={"Title"}
+          isRequire
+          id={"title"}
+          handleChanges={handleInputChange}
+          value={data?.title}
+        />
+        <TextInput
+          label={"Meta Title"}
+          isRequire
+          id={"meta_title"}
+          handleChanges={handleInputChange}
+          value={data?.meta_title}
+        />
+        <TextInput
+          label={"Meta Keywords"}
+          isRequire
+          id={"meta_keywords"}
+          handleChanges={handleInputChange}
+          value={data?.meta_keywords}
+        />
         <TextInput
           label={"Meta Descripion"}
           isRequire
           id={"meta_description"}
+          handleChanges={handleInputChange}
+          value={data?.meta_description}
         />
         <div className="add-menu-input w-1/2 mb-5">
           <CKEditor
@@ -33,8 +52,10 @@ function AddFinancePage() {
               // You can store the "editor" and use when it is needed.
               console.log("Editor is ready to use!", editor);
             }}
-            onChange={(event) => {
-              console.log(event);
+            onChange={(event, editor) => {
+              handleInputChange({
+                target: { value: editor?.getData(), id: "editor_desc" },
+              });
             }}
             onBlur={(event, editor) => {
               console.log("Blur.", editor);
@@ -45,12 +66,21 @@ function AddFinancePage() {
           />
         </div>
         <div className="upload-file-div mb-6 flex justify-between">
-          <Dropzone title={"Bank Image"} />
-          <Dropzone title={"Banner Image"} />
+          <Dropzone
+            title={"Bank Image"}
+            id="bank_image"
+            onChanges={handleInputChange}
+          />
+          <Dropzone
+            title={"Banner Image"}
+            id="banner_image"
+            onChanges={handleInputChange}
+          />
         </div>
 
         <button
           type="button"
+          onClick={handleSubmit}
           className="text-white bg-[#1e6c89] hover:bg-[#164e63] font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
         >
           Save
