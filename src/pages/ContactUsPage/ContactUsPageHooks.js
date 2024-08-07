e966c84ebe8afb529f2452643490751ac04493db
ayush@4718 <ayushhirapara1108@gmail.com>
2024-08-07 18:45:46 +0530

    set data to table of Contact us

--- src/pages/ContactUsPage/ContactUsPageHooks.js
+++ src/pages/ContactUsPage/ContactUsPageHooks.js
@@ -1,13 +1,36 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllContactUsData } from "./action";
+import { IoCheckmarkDoneOutline } from "react-icons/io5";
+import { Phone } from "@mui/icons-material";
 
 export default function AboutPageHooks () {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [tableData, setTableData] = useState();
+
+  const StoreData = useSelector((state) => state?.contactUsDataReducer);
+  console.log("Hello",StoreData.contactUsData?.list);
+
   useEffect(() => {
+    dispatch(getAllContactUsData());
     window.scrollTo(0, 0);
   }, []);
+  useEffect(() => {
+    const td = StoreData?.contactUsData?.list?.map((item,index) => ({
+      ...item,
+      status: item?.isResolved ? "not-active" : "active",
+      Phone: item?.mobile,
+      _id: item._id,
+      no: index,
+      action: <IoCheckmarkDoneOutline className="mx-auto" />,
+    }));
+    setTableData(td ? td : []);
+  }, [StoreData]);
 
   return {
     navigate,
+    tableData,
   };
 }
--- src/pages/ContactUsPage/index.js
+++ src/pages/ContactUsPage/index.js
@@ -3,11 +3,12 @@ import "./index.scss";
 import ContactUsPageHooks from "./ContactUsPageHooks";
 import { Box, Container } from "@mui/material";
 import EnhancedTable from "../../components/Table";
-import { HeaderData, TableData } from "./constant";
+import { HeaderData } from "./constant";
+import { loaderFunc } from "../../utils/helper";
 
 function ContactUsPage() {
-  const { navigate } = ContactUsPageHooks();
-
+  const { navigate, tableData } = ContactUsPageHooks();
+  console.log("hello", tableData);
   return (
     <Box
       className="menu-list-page"
@@ -15,24 +16,24 @@ function ContactUsPage() {
       sx={{ flexGrow: 1, p: 3, mt: 8 }}
     >
       <Container className="pt-[60px] menu-list-container text-left">
-        <EnhancedTable
-          cellData={HeaderData}
-          isActionCol={false}
-          rowItems={[
-            "no",
-            "name",
-            "email",
-            "phone",
-            "subject",
-            "message",
-            "status",
-            "action",
-          ]}
-          rowData={TableData?.map((item, index) => ({
-            ...item,
-            no: 1 + index,
-          }))}
-        />
+        {loaderFunc(
+          tableData,
+          <EnhancedTable
+            cellData={HeaderData}
+            isActionCol={false}
+            rowItems={[
+              "no",
+              "name",
+              "email",
+              "phone",
+              "subject",
+              "message",
+              "status",
+              "action",
+            ]}
+            rowData={tableData}
+          />
+        )}
       </Container>
     </Box>
   );
