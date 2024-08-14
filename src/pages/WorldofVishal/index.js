cfdc93c3a68150b6e929b83c00fcb5d0246d12df
ayush@4718 <ayushhirapara1108@gmail.com>
2024-08-14 17:09:50 +0530

    change redirection

--- src/pages/WorldofVishal/index.js
+++ src/pages/WorldofVishal/index.js
@@ -26,7 +26,7 @@ function WorldofVishal() {
       <Container className="pt-[60px] menu-list-container text-left">
         <button
           type="button"
-          onClick={() => navigate("/add-about-us")}
+          onClick={() => navigate("/add-worldofvishal")}
           className="text-white bg-[#1e6c89] hover:bg-[#164e63] font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-3"
         >
           Add WorldofVishal
