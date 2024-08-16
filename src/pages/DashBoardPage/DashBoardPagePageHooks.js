1367ccc7f61feedfc51291ac72576443ef601b2c
ayush@4718 <ayushhirapara1108@gmail.com>
2024-08-16 18:53:33 +0530

    dashboard hook

--- src/pages/DashBoardPage/DashBoardPagePageHooks.js
+++ src/pages/DashBoardPage/DashBoardPagePageHooks.js
@@ -1,16 +1,20 @@
 import { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { getAllPropertyData } from "../PropertyListPage/action";
 
 export default function DashBoardPagePageHook() {
+  const dispatch = useDispatch();
   const storeData = useSelector((state) => state);
   const navigate = useNavigate();
+  
   useEffect(() => {
     window.scrollTo(0, 0);
+    dispatch(getAllPropertyData());
   }, []);
 
   return {
     navigate,
-    storeData
+    storeData,
   };
 }
