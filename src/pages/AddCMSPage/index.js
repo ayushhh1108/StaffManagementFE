1ce0f1398375312fc1f8807655d9725c07c8bae4
ayush@4718 <ayushhirapara1108@gmail.com>
2024-08-16 18:01:56 +0530

    add CMS page and dummy APIs

--- src/pages/AddCMSPage/index.js
+++ src/pages/AddCMSPage/index.js
@@ -2,14 +2,12 @@ import React from "react";
 import "./index.scss";
 import AddCMSHooks from "./AddCMSHooks";
 import { Box, Typography } from "@mui/material";
-import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
-import Dropzone from "../../components/DropZone";
 import TextInput from "../../components/TextInput";
 import SelectInput from "../../components/SelectInput";
 
 function AddCMSPage() {
-  const { navigate, handleSubmit, handleInputChange, data } = AddCMSHooks();
+  const { navigate, handleSubmit, handleInputChange, data, error } =
+    AddCMSHooks();
 
   return (
     <Box component="main" sx={{ flexGrow: 1, p: 3, mt: 8, maxWidth: "100%" }}>
@@ -17,98 +15,26 @@ function AddCMSPage() {
         <Typography variant="h5" className="mb-5 form-label">
           Add CMS{" "}
         </Typography>
-        <SelectInput
-          label={"Position"}
+        <TextInput
+          label={"Name"}
           isRequire
-          id={"position"}
-          handleChange={handleInputChange}
-          value={data?.position}
-          options={[
-            { label: "Left", value: "left" },
-            { label: "Right", value: "right" },
-          ]}
+          id={"name"}
+          handleChanges={handleInputChange}
+          value={data?.name}
+          isError={error?.name}
         />
         <SelectInput
-          label={"Type"}
+          label={"Select Status"}
           isRequire
-          id={"type"}
+          id={"status"}
           handleChange={handleInputChange}
-          value={data?.type}
+          value={data?.status}
           options={[
-            { label: "Page", value: "page" },
-            { label: "Url", value: "url" },
+            { label: "Active", value: "active" },
+            { label: "Inactive", value: "inactive" },
           ]}
+          isError={error?.status}
         />
-        <TextInput
-          label={"Page Name"}
-          isRequire
-          id={"page_name"}
-          handleChanges={handleInputChange}
-          value={data?.page_name}
-        />
-        <TextInput
-          label={"Page Title"}
-          isRequire
-          id={"page_title"}
-          handleChanges={handleInputChange}
-          value={data?.page_title}
-        />
-        <TextInput
-          label={"Short Description"}
-          isRequire
-          id={"short_desc"}
-          handleChanges={handleInputChange}
-          value={data?.short_desc}
-        />
-        <TextInput
-          label={"Meta Title "}
-          isRequire
-          id={"meta_title"}
-          handleChanges={handleInputChange}
-          value={data?.meta_title}
-        />
-        <TextInput
-          label={"Meta Keywords "}
-          isRequire
-          id={"meta_keywords"}
-          handleChanges={handleInputChange}
-          value={data?.meta_keywords}
-        />
-        <TextInput
-          label={"Meta Description "}
-          isRequire
-          id={"meta_description"}
-          handleChanges={handleInputChange}
-          value={data?.meta_description}
-        />
-        <div className="add-menu-input w-1/2 mb-5">
-          <CKEditor
-            editor={ClassicEditor}
-            data={data?.editor_desc}
-            onReady={(editor) => {
-              // You can store the "editor" and use when it is needed.
-              console.log("Editor is ready to use!", editor);
-            }}
-            onChange={(event, editor) => {
-              handleInputChange({
-                target: { value: editor?.getData(), id: "editor_desc" },
-              });
-            }}
-            onBlur={(event, editor) => {
-              console.log("Blur.", editor);
-            }}
-            onFocus={(event, editor) => {
-              console.log("Focus.", editor);
-            }}
-          />
-        </div>
-        <div className="upload-file-div mb-6 flex justify-between">
-          <Dropzone
-            title={"Icon Image"}
-            id="icon_image"
-            onChanges={handleInputChange}
-          />
-        </div>
 
         <button
           type="button"
