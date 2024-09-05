be3970668f8a644cf48a28ea7dd3f9502411c5e2
ayush@4718 <ayushhirapara1108@gmail.com>
2024-09-05 18:27:29 +0530

    export to execel

--- src/pages/BookingsPage/index.js
+++ src/pages/BookingsPage/index.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
+import * as XLSX from "xlsx";
 import "./index.scss";
 import BookingsPageHook from "./BookingsPageHook";
 import { Box, Container, Typography } from "@mui/material";
@@ -10,6 +11,21 @@ import DeleteDialog from "../../components/DeleteDialog";
 function BookingsPage() {
   const { tableData, open, setOpen, handleConfirmDelete } = BookingsPageHook();
 
+  const exportToExcel = useCallback(() => {
+    const dataToExport = tableData.map((item, index) => ({
+      No: index + 1,
+      PropertyName: item.property_name,
+      Name: item.name,
+      Email: item.email,
+      Mobile: item.mobile,
+      Status: item.status,
+    }));
+    const worksheet = XLSX.utils.json_to_sheet(dataToExport);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Bookings");
+    XLSX.writeFile(workbook, "BookingList.xlsx");
+  }, [tableData]);
+
   return (
     <Box
       className="menu-list-page"
@@ -20,6 +36,13 @@ function BookingsPage() {
         <Typography variant="h5" className="mb-5 form-label text-left">
           Bookings List{" "}
         </Typography>
+        <button
+          type="button"
+          onClick={exportToExcel}
+          className="text-white bg-[#1e6c89] hover:bg-[#164e63] font-medium rounded-lg text-sm px-5 py-2.5 my-3 ml-3"
+        >
+          Export to Excel
+        </button>
         {loaderFunc(
           tableData,
           <EnhancedTable
