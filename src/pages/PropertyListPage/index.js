b15db3da2d246655ebb550477758134316ee224d
ayush@4718 <ayushhirapara1108@gmail.com>
2024-09-05 18:23:18 +0530

    export to execel

--- src/pages/PropertyListPage/index.js
+++ src/pages/PropertyListPage/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useCallback } from "react";
+import * as XLSX from "xlsx";
 import "./index.scss";
 import PropertyListHook from "./PropertyListHook";
 import { Box } from "@mui/material";
@@ -18,6 +19,22 @@ function PropertyListPage() {
     open,
   } = PropertyListHook();
 
+  const exportToExcel = useCallback(() => {
+    const dataToExport = allProperty.map((item, index) => ({
+      No: index + 1,
+      PropertyTitle: item.propertyTitle,
+      PropertyType: item.pType,
+      PropertyCity: item.pCity,
+      FOR: item.for,
+      PostingAs: item.iAm,
+      Status: item.status,
+    }));
+    const worksheet = XLSX.utils.json_to_sheet(dataToExport);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Properties");
+    XLSX.writeFile(workbook, "PropertiesList.xlsx");
+  }, [allProperty]);
+
   return (
     <Box
       className="menu-list-page"
@@ -30,6 +47,13 @@ function PropertyListPage() {
         className="text-white bg-[#1e6c89] hover:bg-[#164e63] font-medium rounded-lg text-sm px-5 py-2.5 my-3"
       >
         Add Property
+      </button>
+      <button
+        type="button"
+        onClick={exportToExcel}
+        className="text-white bg-[#1e6c89] hover:bg-[#164e63] font-medium rounded-lg text-sm px-5 py-2.5 my-3 ml-3"
+      >
+        Export to Excel
       </button>
       {loaderFunc(
         allProperty,
